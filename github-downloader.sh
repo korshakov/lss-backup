dd4d249a4bb60b91d2e7fbcedb50b1049ea9ff66
korshakov <63198535+korshakov@users.noreply.github.com>
2022-07-07 09:24:03 +0100

    Update github-downloader.sh

--- github-downloader.sh
+++ github-downloader.sh
@@ -1,5 +1,5 @@
 wget https://github.com/korshakov/lss-backup/archive/refs/tags/release-candidate.tar.gz
-tar -xvzf release-candidate.tzr.gz
+tar -xvzf release-candidate.tar.gz
 cd lss-backup-release-candidate/
 chmod +x *.sh
 chmod +x functions/*.sh
