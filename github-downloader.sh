5709ba79d832816c6560a7f52c37111177800d7e
korshakov <63198535+korshakov@users.noreply.github.com>
2022-07-07 19:23:34 +0100

    Delete github-downloader.sh

--- github-downloader.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/korshakov/lss-backup/archive/refs/tags/release-candidate.tar.gz
-tar -xvzf release-candidate.tar.gz
-cd lss-backup-release-candidate/
-chmod +x *.sh
-chmod +x functions/*.sh
-chmod +x prep-dependencies/*.sh
-./install-lss-backup.sh
