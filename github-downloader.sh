ae388e6431588d058e48dae73ad5cd954b231de1
korshakov <stojanikl@gmail.com>
2022-07-07 19:00:13 +0100

    Update github-downloader.sh

--- github-downloader.sh
+++ github-downloader.sh
@@ -1,7 +1,14 @@
 wget https://github.com/korshakov/lss-backup/archive/refs/tags/release-candidate.tar.gz
-tar -xvzf release-candidate.tar.gz
-cd lss-backup-release-candidate/
+SETUPWORKDIR=$(pwd)
+
+tar -xvzf $SETUPWORKDIR/release-candidate.tar.gz $SETUPWORKDIR/lss-backup
+cd $SETUPWORKDIR/lss-backup
 chmod +x *.sh
-chmod +x functions/*.sh
-chmod +x prep-dependencies/*.sh
+chmod +x ./functions/*.sh
+chmod +x ./prep-dependencies/*.sh
 ./install-lss-backup.sh
+
+
+# curl command
+curl -o github-downloader.sh https://lssolutions.ie/lss-backup/github-downloader.sh
+bash ./github-downloader.sh
