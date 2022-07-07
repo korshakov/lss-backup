23005a9f65d71b669df38d50fe8e79813a82c88d
korshakov <stojanikl@gmail.com>
2022-07-07 14:05:51 +0100

    Amending init only
    There is no need to check for snapshots before init. Restic will tell us if its initialized already or not.

--- functions/restic-repo-init-only.sh
+++ functions/restic-repo-init-only.sh
@@ -5,15 +5,8 @@ export RESTIC_PASSWORD="$RESTIC_PASSWORD"
 export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 export AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION"
-restic -r $LSS_REPOSITORY snapshots
-EXITCODE="$?"
-if [ "$EXITCODE" -eq "0" ]
-then
-# Repository exist already we can move onto actual data backup.
-echo "Repository already exist! There is no need to initialize again!"
-exit
-else
+
+
 echo "Initializing restic repository only, first backup will happen on the nearest schedule date and time."
 restic -r $LSS_REPOSITORY init
 echo "Restic repository initialization complete."
-fi
