47e18c6df269e407ceb228a7a579aec5f89b20af
korshakov <63198535+korshakov@users.noreply.github.com>
2022-07-07 08:24:57 +0100

    Update lss-reten-keep-last-only.sh

--- functions/lss-reten-keep-last-only.sh
+++ functions/lss-reten-keep-last-only.sh
@@ -7,4 +7,4 @@ restic -r $LSS_REPOSITORY forget --keep-last $RESTIC_FORGETLAST
 # Moving to pruning
 echo "Mooving to pruning"
 
-# /bin/bash "$WORKDIR"/"$BKID"-lss-prune.sh
+/bin/bash "$WORKDIR"/"$BKID"-lss-prune.sh
