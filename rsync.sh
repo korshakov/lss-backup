74a819d075e82d58b5406c78e54c23ff85892d5f
korshakov <stojanikl@gmail.com>
2022-07-15 07:59:33 +0100

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -276,8 +276,6 @@ SETUPWORKDIR=$(pwd)
 echo "WORKDIR=$SETUPWORKDIR/database/backup-jobs/$SETUPBKID" >> ./database/backup-jobs/"$SETUPBKID"/"$SETUPBKID-Configuration.env"
 echo "Your backup configuration files will be stored at: $SETUPWORKDIR/database/backup-jobs/$SETUPBKID"
 
-echo "LOG_FILE=$SETUPWORKDIR/database/backup-jobs/$SETUPBKID/logs/$SETUPTIMESTAMP-$SETUPBKID.log" >> ./database/backup-jobs/"$SETUPBKID"/"$SETUPBKID-Configuration.env"
-
 #create executable file for cronjob starter file.sh
 
 cp ./functions/source-type-checks.sh ./database/backup-jobs/"$SETUPBKID"/$SETUPBKID-source-type-checks.sh
