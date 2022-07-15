51cb71594751213da21dbf62707e3eedbfc382cd
korshakov <stojanikl@gmail.com>
2022-07-15 10:34:43 +0100

    Update log-cleanup.sh

--- functions/log-cleanup.sh
+++ functions/log-cleanup.sh
@@ -5,6 +5,6 @@ echo "Looking for log files older than 2 weeks"
 echo "Found these:"
 find $WORKDIR/logs/* -type f -mmin +20160
 echo "Deleting selected files older than 2 weeks from above list."
-find $LOGDIR/* -type f -mmin +20160 -exec rm {} \;
+find $LOGDIR/* -type f -mmin +20160 -exec rm -rf {} \;
 echo "Done"
 wget "$CRONDOMAIN"/ping/"$CRONID" -T 10 -t 5 -O /dev/null
