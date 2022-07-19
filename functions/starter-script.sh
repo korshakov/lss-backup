b512d4833a4c9fb6e9466fc2b596bd1cdf6500b5
korshakov <stojanikl@gmail.com>
2022-07-19 11:11:10 +0100

    Update starter-script.sh

--- functions/starter-script.sh
+++ functions/starter-script.sh
@@ -2,7 +2,7 @@
 
 echo "Welcome to LS Solutions Backup!"
 
-        	export STATUS=start
-            /bin/bash "$WORKDIR"/"$BKID"-healthchecks.sh
+export STATUS=start
+/bin/bash "$WORKDIR"/"$BKID"-healthchecks.sh
 
 /bin/bash "$WORKDIR"/"$BKID"-backup-config-check.sh
