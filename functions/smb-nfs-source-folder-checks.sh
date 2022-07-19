eeec1deb54dd2cdfee84b88ce4e0c98c880497ee
korshakov <stojanikl@gmail.com>
2022-07-19 06:36:35 +0100

    Update smb-nfs-source-folder-checks.sh

--- functions/smb-nfs-source-folder-checks.sh
+++ functions/smb-nfs-source-folder-checks.sh
@@ -6,7 +6,12 @@ if find "$SDIR" -mindepth 1 -maxdepth 1 | read; then
    /bin/bash "$WORKDIR"/"$BKID"-destination-type-checks.sh
    exit
 else
-   echo "Warning SMB/NFS source directory is empty! There is nothing to backup! Sending failed ping and exitting backup process."
+   echo "Warning SMB/NFS source directory is empty! There is nothing to backup! Aborting backup process."
+   if [[ $MONITORING == 'NO' ]]
+   then
+   echo "Healthchecks monitoring disabled."
+   else
    wget "$CRONDOMAIN"/ping/"$CRONID"/13 -T 10 -t 5 -O /dev/null
+   fi
    exit
 fi
