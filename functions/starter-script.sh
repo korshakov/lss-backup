e353f62dadceec821d4bdd123b7820beb31a2b21
korshakov <stojanikl@gmail.com>
2022-07-19 06:14:00 +0100

    Update starter-script.sh

--- functions/starter-script.sh
+++ functions/starter-script.sh
@@ -2,6 +2,11 @@
 
 echo "Welcome to LS Solutions Backup!"
 
+if [[ $MONITORING == 'NO' ]]
+then
+echo "Healthchecks monitoring disabled."
+else
 wget "$CRONDOMAIN"/ping/"$CRONID"/start -T 10 -t 5 -O /dev/null
+fi
 
 /bin/bash "$WORKDIR"/"$BKID"-backup-config-check.sh
