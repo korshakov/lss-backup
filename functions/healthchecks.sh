0909ca050fbec3dd434191ac5b7bfc7efe7ed097
korshakov <stojanikl@gmail.com>
2022-07-22 09:18:12 +0100

    Update healthchecks.sh

--- functions/healthchecks.sh
+++ functions/healthchecks.sh
@@ -3,5 +3,5 @@
 if [[ $MONITORING == "NO" ]]; then
 echo "Monitoring is disabled"
 else
-wget https://cron.lssolutions.ie/ping/8bfc3d73-d49e-427c-8e70-8e40a7d67f1d/"$STATUS" -T 10 -t 5 -O /dev/null
+wget "$CRONDOMAIN"/ping/"CRONID"/"$STATUS" -T 10 -t 5 -O /dev/null
 fi
