2099babfcfa551fae9d77bb20b29a27e35fc91c4
korshakov <63198535+korshakov@users.noreply.github.com>
2023-04-09 17:31:47 +0100

    Update backup-wizard.sh

--- backup-wizard.sh
+++ backup-wizard.sh
@@ -4,12 +4,14 @@ echo "Copyright - Ladislav Stojanik - LS Solutions - https://lssolutions.ie"
 echo "#####################################################################"
 echo ""
 echo "Which program would you like to use? Type RESTIC or RSYNC pres CTRL+C to exit program."
-select PROGRAM in "RESTIC" "RSYNC"; do
+select PROGRAM in "RESTIC" "RSYNC" "EXIT"; do
     case $PROGRAM in
 
         RESTIC ) clear; ./restic.sh ; break;;
 
-        RSYNC ) clear; ./rsync.sh ; exit;;
+        RSYNC ) clear; ./rsync.sh ; break;;
+
+	EXIT ) clear && echo "Good Bye!"; exit;;
 
     esac
 done
