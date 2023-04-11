5079bece15f3e0ff5450993740991a91e0d0dead
korshakov <63198535+korshakov@users.noreply.github.com>
2023-04-12 00:02:22 +0100

    Update cron-remove.sh

--- functions/cron-remove.sh
+++ functions/cron-remove.sh
@@ -2,7 +2,7 @@
 
 if [[ $BKFQ == 'Daily' ]] || [[ $BKFQ == 'Weekly' ]] || [[ $BKFQ == 'Monthly' ]]
 then
-crontab -u root -l | grep -v "$BKID"  | crontab -u root -
+crontab -u root -l | sed -n '/'"$BKID"'-'"$BKFQ"'-'"$BKNAME"'/,/'"$BKID"'-'"$BKFQ"'-'"$BKNAME"'.sh/!p'
 service cron reload
 else
 echo "Backup job was Manual Only, nothing to remove from crontab."
