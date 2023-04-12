39de4fd468a7b1990719c06d336e31fc4403376a
korshakov <63198535+korshakov@users.noreply.github.com>
2023-04-12 20:06:45 +0100

    Update healthchecks.sh

--- functions/healthchecks.sh
+++ functions/healthchecks.sh
@@ -5,3 +5,16 @@ echo "Monitoring is disabled"
 else
 wget "$CRONDOMAIN"/ping/"$CRONID"/"$STATUS" -T 10 -t 5 -O /dev/null
 fi
+
+if [[ $EMAILSETUP == "FailOnly" ]]; then
+
+subject="$BKID-$BKFQ-$BKNAME Has Failed with status code: $STATUS"
+
+body="$PROGRAM backup job $BKID-$BKFQ-$BKNAME has failed with exit code: $STATUS. "
+
+to="$EMAILSETUPADDR"
+
+echo -e "Subject:${subject}\n${body}" | sendmail -t "${to}"
+
+fi
+
