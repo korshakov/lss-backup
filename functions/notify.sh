5a9ccb5d38f10d8783b05213f3d59cb239bef65e
korshakov <63198535+korshakov@users.noreply.github.com>
2023-04-12 20:57:16 +0100

    Update notify.sh

--- functions/notify.sh
+++ functions/notify.sh
@@ -6,7 +6,9 @@ else
 wget "$CRONDOMAIN"/ping/"$CRONID"/"$STATUS" -T 10 -t 5 -O /dev/null
 fi
 
-if [[ $EMAILSETUP == "FailOnly" ]]; then
+if [[ $EMAILSETUP == "Yes" ]] || [[ $EMAILSETUP == "FailOnly" ]] ; then
+
+if [[ $STATUS != '0' ]] then
 
 subject="$BKID-$BKFQ-$BKNAME Has Failed with status code: $STATUS"
 
@@ -16,5 +18,20 @@ to="$EMAILSETUPADDR"
 
 echo -e "Subject:${subject}\n${body}" | sendmail -t "${to}"
 
-fi
 
+
+else
+
+subject="$BKID-$BKFQ-$BKNAME Has finished sucessfully."
+
+body="$PROGRAM backup job $BKID-$BKFQ-$BKNAME has finished sucessfully. "
+
+to="$EMAILSETUPADDR"
+
+echo -e "Subject:${subject}\n${body}" | sendmail -t "${to}"
+
+
+fi
+else
+echo "Email notifications are disabled."
+fi
