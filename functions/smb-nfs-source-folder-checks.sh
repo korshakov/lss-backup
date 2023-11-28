6200dc401b7fc6d9423b3ac53721a918cb7566b4
korshakov <63198535+korshakov@users.noreply.github.com>
2023-11-28 17:01:05 +0000

    Update smb-nfs-source-folder-checks.sh

--- functions/smb-nfs-source-folder-checks.sh
+++ functions/smb-nfs-source-folder-checks.sh
@@ -8,6 +8,6 @@ if find "$SDIR" -mindepth 1 -maxdepth 1 | read; then
 else
    echo "Warning SMB/NFS source directory is empty! There is nothing to backup! Sending failed ping and exitting backup process."
         	export STATUS=13
-            /bin/bash "$WORKDIR"/"$BKID"-healthchecks.sh
+            /bin/bash "$WORKDIR"/"$BKID"-notify.sh
    exit
 fi
