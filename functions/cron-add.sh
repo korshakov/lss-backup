0b600ebfe51fd1f7c1af40844eabe1637b90628e
korshakov <63198535+korshakov@users.noreply.github.com>
2023-12-02 15:58:05 +0000

    Update cron-add.sh

--- functions/cron-add.sh
+++ functions/cron-add.sh
@@ -9,7 +9,7 @@ echo "Injecting command to crontab to the last line."
 echo "Here is is for your convinience: $BKCRONTIMEMM $BKCRONTIMEHH * * * /bin/bash $WORKDIR/$BKID-$BKFQ-$BKNAME.sh"
 line1="### Cron for $BKID-$BKFQ-$BKNAME"
 line2="MAILTO=\"\""
-line3="$BKCRONTIMEMM $BKCRONTIMEHH * * * /bin/bash $WORKDIR/$BKID-$BKFQ-$BKNAME.sh"
+line3="$BKCRONTIMEMM $BKCRONTIMEHH * * $BKCRONDAYS /bin/bash $WORKDIR/$BKID-$BKFQ-$BKNAME.sh"
 
 (crontab -u root -l; echo "" ) | crontab -u root -
 (crontab -u root -l; echo "$line1" ) | crontab -u root -
